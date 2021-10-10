f40211ae86e23459435372767879b1e94b12af57
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-10 12:50:27 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+>>> student_tuples = [
+...     ('john', 'A', 15),
+...     ('jane', 'B', 12),
+...     ('dave', 'B', 10),
+... ]
+>>> sorted(student_tuples, key=lambda student: student[2])   # sort by age
+[('dave', 'B', 10), ('jane', 'B', 12), ('john', 'A', 15)]
